780c626c60d46bc0f34e96e1d25b6c3a3503e821
Tetiana.Boiar <taniavasylchyk@gmail.com>
2023-04-06 01:51:02 +0300

    Updates

--- src/api/fetchDetails.ts
+++
@@ -1,8 +0,0 @@
-import { PokemonDetails } from '../types/PokemonDetails';
-import { BASE_URL, client } from './fetchClient';
-
-
-export const getDetails = (url: string) => {
-  const endpoint = url.replace(BASE_URL, '');
-  return client.get<PokemonDetails>(endpoint);
-}; 
--- src/types/ErrorMessages.ts
+++
@@ -1,4 +0,0 @@
-export enum ErrorMessages {
-  OnLoad = 'Something went wrong',
-  OnEmptyData = 'There are no pokemons yet',
-}
